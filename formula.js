3cb3431e252a22e034956666cc9129a158891ab3
HikaruPractice <119712253+HikaruPractice@users.noreply.github.com>
2023-04-03 11:39:33 +0900

    Add files via upload

--- formula.js
+++ formula.js
@@ -15,9 +15,9 @@ function feeRakuma(price) {
     return Math.floor(price * 0.066);
 }
 function priceRakuma(amount) {
-    const b = [15,29,43,57,71,85,100,114,128,142,156,170,184,199,213,227,241,255,269,284,298,312,326,340,354,368,383,397,411,425,439,453,467    ]
-    let d =Math.floor(amount / 468);
-    let m = amount - d * 468;
+    const b = [15,29,43,57,71,85,100,114,128,142,156,170,184,199,213,227,241,255,269,284,298,312,326,340,354,368,383,397,411,425,439,453,467]
+    let d =Math.floor(amount / 467);
+    let m = amount - d * 467;
     for (let i=0;i<33;i++){
         if (m <= b[i]){
             return d*33+i+amount;
